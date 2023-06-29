d9e96369c15fe0de12550c5111fe13f308e06272
Ashe-png <ashish.khatakho11@gmail.com>
2023-06-29 18:00:10 +0545

    first commit

---
+++ components/Provider.jsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { SessionProvider, getSession } from 'next-auth/react';
+
+const Provider = ({ children, session }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+};
+export default Provider;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    esmExternals: 'loose', // <-- add this
+    serverComponentsExternalPackages: ['mongoose'], // <-- and this
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
